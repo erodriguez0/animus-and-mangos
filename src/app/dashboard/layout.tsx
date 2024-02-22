c195564af4d137c6ae428b6fe3cd453b10a0b0c4
erodriguez0 <erodriguez0750@gmail.com>
2024-02-22 12:06:45 -0800

    Hide double theme toggle on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -28,9 +28,9 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
       <main className="container mx-auto flex min-h-[calc(100vh-4rem)] gap-2 px-4 lg:min-h-[calc(100vh-5rem)] xl:gap-4 xl:px-0">
         <DashboardSidebar />
         <div className="relative flex flex-1 flex-col py-4">
-          <div className="absolute right-0 top-4 hidden lg:block">
+          {/* <div className="absolute right-0 top-4 hidden lg:block">
             <ThemeToggle />
-          </div>
+          </div> */}
           <div className="flex flex-col gap-4 lg:gap-y-8">{children}</div>
         </div>
       </main>
