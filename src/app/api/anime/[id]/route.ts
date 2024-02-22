88184877d53bfbc133a911c8a9808c3ef12f88c5
erodriguez0 <erodriguez0750@gmail.com>
2024-02-22 14:07:23 -0800

    Anime/Character PATCH routes

--- src/app/api/anime/[id]/route.ts
+++ src/app/api/anime/[id]/route.ts
@@ -1,3 +1,8 @@
+import { UserRole } from "@prisma/client"
+
+import { AnimeSchema } from "@/lib/validators/anime"
+
+import { getAuthSession } from "@/lib/auth"
 import { prismadb } from "@/lib/db"
 
 interface IParams {
@@ -28,6 +33,101 @@ export async function GET(req: Request, { params }: IParams) {
     return Response.json(anime)
   } catch (error) {
     console.log("[ANIME_ID_GET]\n", error)
+
+    return Response.json({ message: "Internal server error" }, { status: 500 })
+  }
+}
+
+export async function PATCH(req: Request, { params }: IParams) {
+  try {
+    const session = await getAuthSession()
+
+    if (!session?.user) {
+      return Response.json({ message: "Unauthenticated" }, { status: 401 })
+    }
+
+    if (session.user.role !== UserRole.ADMIN) {
+      return Response.json({ message: "Unauthorized" }, { status: 403 })
+    }
+
+    const animeExists = await prismadb.anime.findUnique({
+      where: {
+        id: params.id,
+      },
+      include: {
+        characters: true,
+      },
+    })
+
+    if (!animeExists) {
+      return Response.json({ message: "Anime not found" }, { status: 404 })
+    }
+
+    const body = await req.json()
+
+    const parsed = await AnimeSchema.safeParseAsync(body)
+
+    if (!parsed.success) {
+      return Response.json({ message: "Invalid anime data" }, { status: 400 })
+    }
+
+    const { data } = parsed
+
+    const anime = await prismadb.anime.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        title: data.title,
+        poster: data.poster,
+        format: data.format || null,
+        status: data.status || null,
+        episode_count: data.episode_count || null,
+        season: data.season || null,
+        year: data.year || null,
+        source_type: data.source_type || null,
+        age_rating: data.age_rating || null,
+        synopsis: data.synopsis,
+        background: data.background,
+      },
+    })
+
+    // Differentiate between added/deleted anime characters
+    const animeCharacters = animeExists.characters.map(
+      item => item.character_id,
+    )
+    const charactersToDelete = animeCharacters.filter(
+      item => !data.characters.includes(item),
+    )
+    const charactersToCreate = data.characters.filter(
+      item => !animeCharacters.includes(item),
+    )
+
+    if (charactersToDelete.length) {
+      await prismadb.animeCharacter.deleteMany({
+        where: {
+          character_id: {
+            in: charactersToDelete,
+          },
+          anime_id: params.id,
+        },
+      })
+    }
+
+    if (charactersToCreate.length) {
+      await prismadb.animeCharacter.createMany({
+        data: charactersToCreate.map(item => {
+          return {
+            anime_id: params.id,
+            character_id: item,
+          }
+        }),
+      })
+    }
+
+    return Response.json(anime)
+  } catch (error) {
+    console.log("[ANIME_ID_POST]\n", error)
 
     return Response.json({ message: "Internal server error" }, { status: 500 })
   }
--- src/app/api/character/[id]/route.ts
+++ src/app/api/character/[id]/route.ts
@@ -1,3 +1,8 @@
+import { UserRole } from "@prisma/client"
+
+import { CharacterSchema } from "@/lib/validators/character"
+
+import { getAuthSession } from "@/lib/auth"
 import { prismadb } from "@/lib/db"
 
 interface IParams {
@@ -28,6 +33,122 @@ export async function GET(req: Request, { params }: IParams) {
 
     if (!character) {
       return Response.json({ message: "Character not found" }, { status: 404 })
+    }
+
+    return Response.json(character)
+  } catch (error) {
+    console.log("[CHARACTER_ID_GET]\n", error)
+
+    return Response.json({ message: "Internal server error" }, { status: 500 })
+  }
+}
+
+export async function PATCH(req: Request, { params }: IParams) {
+  try {
+    const session = await getAuthSession()
+
+    if (!session?.user) {
+      return Response.json({ message: "Unauthenticated" }, { status: 401 })
+    }
+
+    if (session.user.role !== UserRole.ADMIN) {
+      return Response.json({ message: "Unauthorized" }, { status: 403 })
+    }
+
+    const body = await req.json()
+
+    const parsed = await CharacterSchema.safeParseAsync(body)
+
+    if (!parsed.success) {
+      return Response.json({ message: "Invalid anime data" }, { status: 400 })
+    }
+
+    const { data } = parsed
+
+    const characterExists = await prismadb.character.findUnique({
+      where: {
+        id: params.id,
+      },
+      include: {
+        anime: true,
+        manga: true,
+      },
+    })
+
+    if (!characterExists) {
+      return Response.json({ message: "Character not found" }, { status: 404 })
+    }
+
+    const character = await prismadb.character.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        name: data.name,
+        image: data.image,
+        bio: data.bio,
+        gender: data.gender || null,
+      },
+    })
+
+    // Differentiate between added/deleted anime and manga
+    const animeCharacters = characterExists.anime.map(item => item.anime_id)
+    const mangaCharacters = characterExists.manga.map(item => item.manga_id)
+    const animeToDelete = animeCharacters.filter(
+      item => !data.anime.includes(item),
+    )
+    const mangaToDelete = mangaCharacters.filter(
+      item => !data.manga.includes(item),
+    )
+    const animeToCreate = data.anime.filter(
+      item => !animeCharacters.includes(item),
+    )
+    const mangaToCreate = data.manga.filter(
+      item => !mangaCharacters.includes(item),
+    )
+
+    if (animeToDelete.length) {
+      await prismadb.animeCharacter.deleteMany({
+        where: {
+          anime_id: {
+            in: animeToDelete,
+          },
+          character_id: params.id,
+        },
+      })
+    }
+
+    if (mangaToDelete.length) {
+      await prismadb.mangaCharacter.deleteMany({
+        where: {
+          manga_id: {
+            in: mangaToDelete,
+          },
+          character_id: params.id,
+        },
+      })
+    }
+
+    if (animeToCreate.length) {
+      await prismadb.animeCharacter.createMany({
+        data: animeToCreate.map(item => {
+          return {
+            anime_id: item,
+            character_id: params.id,
+          }
+        }),
+      })
+    }
+
+    if (mangaToCreate.length) {
+      await prismadb.mangaCharacter.createMany({
+        data: mangaToCreate.map(item => {
+          return {
+            manga_id: item,
+            character_id: params.id,
+          }
+        }),
+      })
     }
 
     return Response.json(character)
