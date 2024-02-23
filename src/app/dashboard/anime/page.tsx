d664f6940b9e4206c12ef50d40e2d4347c736ded
erodriguez0 <erodriguez0750@gmail.com>
2024-02-23 03:44:41 -0800

    Anime/Manga list. Global radius.

--- src/app/dashboard/anime/page.tsx
+++ src/app/dashboard/anime/page.tsx
@@ -1,9 +1,59 @@
+import List from "@/components/ui/list"
+import Poster from "@/components/ui/poster"
+
 import DashboardPageTitle from "@/components/dashboard/dashboard-page-title"
 
-const DashboardAnimeOverviewPage = () => {
+import { prismadb } from "@/lib/db"
+
+const DashboardAnimeOverviewPage = async () => {
+  const recent = await prismadb.anime.findMany({
+    take: 3,
+    orderBy: {
+      created_at: "desc",
+    },
+  })
+
+  const updated = await prismadb.anime.findMany({
+    where: {
+      id: {
+        notIn: recent.map(item => item.id),
+      },
+    },
+    take: 3,
+    orderBy: {
+      updated_at: "desc",
+    },
+  })
+
   return (
     <>
       <DashboardPageTitle title="Anime: Overview" />
+
+      <div className="flex flex-col gap-4 lg:flex-row">
+        <div className="w-full lg:w-1/3">
+          <List
+            title="Recently Added"
+            type="anime"
+            items={recent}
+          />
+        </div>
+
+        <div className="w-full lg:w-1/3">
+          <List
+            title="Updated"
+            type="anime"
+            items={updated}
+          />
+        </div>
+
+        <div className="w-full lg:w-1/3">
+          <List
+            title="Updated"
+            type="anime"
+            items={updated}
+          />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/ui/list.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { Anime, Manga } from "@prisma/client"
+import { ChevronDownIcon, EditIcon } from "lucide-react"
+import Link from "next/link"
+import { useState } from "react"
+
+import Poster from "@/components/ui/poster"
+
+import { cn } from "@/lib/utils"
+
+import { Button, buttonVariants } from "./button"
+
+interface ListProps {
+  title?: string
+  type: "anime" | "manga"
+  items: (Anime | Manga)[]
+}
+
+const List = ({ title, type, items }: ListProps) => {
+  const [open, setOpen] = useState(true)
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full flex-col gap-4">
+        <div className="flex h-10 w-full items-center justify-between rounded-md bg-secondary px-2 text-secondary-foreground lg:px-4">
+          <div>
+            <h4 className="line-clamp-1 break-all font-semibold">{title}</h4>
+          </div>
+
+          <Button
+            size="icon"
+            variant="ghost"
+            onClick={() => setOpen(!open)}
+          >
+            <ChevronDownIcon
+              className={cn(
+                "h-4 w-4 transition-transform",
+                !open && "-rotate-90",
+              )}
+            />
+          </Button>
+        </div>
+
+        {items.length > 0 && (
+          <div className={cn("flex flex-col gap-2", !open && "hidden")}>
+            {items.map(item => (
+              <div
+                key={item.id}
+                className="flex w-full rounded-md bg-secondary/25 dark:bg-secondary/50"
+              >
+                <Link
+                  href={`/${type}/${item.id}`}
+                  className="h-20"
+                >
+                  <Poster
+                    src={item?.poster || ""}
+                    iconSize={4}
+                    className="h-full w-auto"
+                  />
+                </Link>
+
+                <Link
+                  href={`/${type}/${item.id}`}
+                  className="flex h-20 w-full flex-wrap p-2"
+                >
+                  <div>
+                    <p className="line-clamp-3 break-all text-sm">
+                      {item?.title}
+                    </p>
+                  </div>
+                </Link>
+
+                <Link
+                  href={`/dashboard/${type}/${item.id}`}
+                  className={cn(
+                    buttonVariants({ size: "icon", variant: "secondary" }),
+                    "ml-auto h-20 shrink-0",
+                  )}
+                >
+                  <EditIcon className="h-4 w-4" />
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default List
